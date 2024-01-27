ef9ded4d189950680c9ae6158adbf5b285102871
Harshana Senadeera <116424581+HarshanaSenadeera@users.noreply.github.com>
2024-01-27 23:45:18 +0530

    Added side bar, name , logo and search bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     extend: {
       colors:{
         customgray :'rgba(59, 58, 58, 1)',
-        customblack :'rgba(31, 29, 43, 1)'
+        customblack :'rgba(31, 29, 43, 1)',
+        homeicon :'rgba(234, 124, 105, 1)',
+      },
+      height :{
+        sidebar: [500]
       }
     },
   },
