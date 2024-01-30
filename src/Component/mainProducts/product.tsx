33f38844b7714dfc1e4e72686012327660ba7343
Harshana Senadeera <116424581+HarshanaSenadeera@users.noreply.github.com>
2024-01-30 16:00:36 +0530

    create product component

--- src/Component/mainProducts/product.tsx
+++ src/Component/mainProducts/product.tsx
@@ -1,15 +1,14 @@
 export default function Product() {
 
-    return(
+    return (
 
-        <div>
-            <div className="w-52 h-60 bg-dishes rounded-md flex justify-center">
-                <div className="bg-white rounded-full w-20 h-20 mb-32">
+        <div className="relative">
+            <div className="w-52 h-32 bg-dishes rounded-md flex justify-center absolute">
+                <div className="bg-white rounded-full w-20 h-20 mb-32 top-0 justify-center ">
 
                 </div>
             </div>
         </div>
-
 
     )
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Main from "../Component/main/main";
 import Product from "../Component/mainProducts/product";
+import Example from "../Component/selectedProducts/product";
 function App() {
   return (
    <>
 
-       <Main/>
-      {/* <Product/>*/}
+       {/*<Main/>*/}
+       {/*<Product/>*/}
+       <Example/>
    </>
   );
 }
